70f63c206fc5c2c66612333e5b1e5e54b19e6a21
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-07 16:48:02 -0500

    Se instalo sweetalert2 para mostrar info de manera mas comoda

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,7 @@ import {
   LOGUEAR_USUARIO_TOKEN,
   REGISTRO_USUARIO,
 } from "@/services/auth";
-
+import Swal from "sweetalert2";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -32,13 +32,19 @@ export default new Vuex.Store({
     },
     loguearUsuarioToken: async ({ commit }) => {
       const token = JSON.parse(<string>localStorage.getItem("token"));
-      //console.log(JSON.stringify(token));
-      await LOGUEAR_USUARIO_TOKEN(token.refreshToken).then(async (result) => {
-        token.idToken = await result.access_token;
-        token.refreshToken = await result.refresh_token;
-        await localStorage.setItem("token", JSON.stringify(token));
-        //console.log(JSON.stringify(token));
-      });
+      if (token !== null) {
+        await LOGUEAR_USUARIO_TOKEN(token.refreshToken).then(async (result) => {
+          token.idToken = await result.access_token;
+          token.refreshToken = await result.refresh_token;
+          await localStorage.setItem("token", JSON.stringify(token));
+        });
+      } else {
+        await Swal.fire(
+          "Vuelve a iniciar sesion",
+          "Por motivos de seguridad vuelve a ingresar tus credenciales",
+          "warning"
+        );
+      }
     },
   },
   modules: {},
