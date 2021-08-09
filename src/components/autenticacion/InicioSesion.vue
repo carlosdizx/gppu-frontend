0bc45b47bbcd9d7c6d8598b8ed4582aa191d575b
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-09 16:29:43 -0500

    Preparado para crear el login

---
+++ src/components/autenticacion/InicioSesion.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-card class="mx-auto my-auto">
+        <v-card-title>
+          <v-img />
+        </v-card-title>
+      </v-card>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "InicioSesion",
+};
+</script>
+
+<style scoped></style>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,12 +1,14 @@
 import Vue from "vue";
-import Vuetify from "vuetify";
-import "vuetify/dist/vuetify.min.css";
+import Vuetify from "vuetify/lib/framework";
 import es from "vuetify/src/locale/es";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
+    options: {
+      customProperties: true,
+    },
     themes: {
       light: {
         primary: "#007BFF",
