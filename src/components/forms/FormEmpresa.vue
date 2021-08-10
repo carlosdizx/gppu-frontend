ce3873baa61e0e342202c3ba109465029db95836
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-09 19:49:28 -0500

    Completo los campos para el form de empresa

---
+++ src/components/forms/FormEmpresa.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-card max-width="800" class="mx-auto my-auto">
+    <v-card-title>Formulario empresa</v-card-title>
+    <v-card-subtitle>
+      Tenga preparado los documentos y datos necesarios
+    </v-card-subtitle>
+    <v-card-text>
+      <v-form autocomplete="off">
+        <v-text-field color="indigo" label="NIT" prepend-icon="mdi-domain" />
+        <v-text-field
+          color="indigo"
+          label="Nombre"
+          prepend-icon="mdi-rename-box"
+        />
+        <v-text-field
+          color="indigo"
+          label="Numero de documento del representante"
+          prepend-icon="mdi-card-account-details"
+        />
+        <v-file-input
+          color="indigo"
+          accept="image/jpeg,image/gif,image/png,application/pdf,image/x-eps"
+          label="Archivo del documento del representante"
+        />
+        <v-file-input
+          color="indigo"
+          accept="image/jpeg,image/gif,image/png,application/pdf,image/x-eps"
+          label="RUT"
+        />
+        <v-file-input
+          color="indigo"
+          accept="image/jpeg,image/gif,image/png,application/pdf,image/x-eps"
+          label="Camara de comercio (opcional)"
+        />
+        <v-text-field
+          color="indigo"
+          label="Numero de celular"
+          type="number"
+          prepend-icon="mdi-card-account-details"
+        />
+        <v-text-field
+          color="indigo"
+          label="Correo electronico"
+          prepend-icon="mdi-email"
+        />
+        <v-divider />
+        <v-text-field color="indigo" label="Pais" prepend-icon="mdi-earth" />
+        <v-text-field
+          color="indigo"
+          label="Departamento/estado/provincia"
+          prepend-icon="mdi-sign-real-estate"
+        />
+        <v-text-field color="indigo" label="Ciudad" prepend-icon="mdi-city" />
+        <v-text-field
+          type="number"
+          color="indigo"
+          label="Codigo postal"
+          prepend-icon="mdi-postage-stamp"
+        />
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "FormEmpresa",
+};
+</script>
+
+<style scoped></style>
--- src/components/toolbars/ToolbarRegistro.vue
+++ src/components/toolbars/ToolbarRegistro.vue
@@ -1,12 +1,19 @@
 <template>
-  <Toolbar titulo="Formularios de registro" icono="mdi-account-multiple-plus" />
+  <div>
+    <Toolbar
+      titulo="Formularios de registro"
+      icono="mdi-account-multiple-plus"
+    />
+    <FormEmpresa />
+  </div>
 </template>
 
 <script lang="ts">
 import Toolbar from "@/components/general/Toolbar.vue";
+import FormEmpresa from "@/components/forms/FormEmpresa.vue";
 export default {
   name: "ToolbarRegistro",
-  components: { Toolbar },
+  components: { Toolbar, FormEmpresa },
 };
 </script>
 
