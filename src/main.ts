1572b7d6179732360978e3e733e7056f99ab1a2d
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-09 22:13:14 -0500

    se configuro firebase

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,25 @@ import "@babel/polyfill";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
 
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDO8hVE9rxb9oP38kTH4qRhqxUcHNtAi3w",
+  authDomain: "gppu-backend.firebaseapp.com",
+  databaseURL: "https://gppu-backend-default-rtdb.firebaseio.com",
+  projectId: "gppu-backend",
+  storageBucket: "gppu-backend.appspot.com",
+  messagingSenderId: "512100142634",
+  appId: "1:512100142634:web:b8a6d707db524a553e3d53",
+  measurementId: "G-H1PD88776G",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 Vue.config.productionTip = false;
+
+export const STORAGE = firebase.storage();
 
 new Vue({
   router,
