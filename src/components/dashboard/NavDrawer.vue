ae22fb0dd9e120606bc012221434189b9c1d3958
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-16 13:28:29 -0500

    Creado el NavDrawer, falta agregar dinamismo para interactuar con el usuario

---
+++ src/components/dashboard/NavDrawer.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-navigation-drawer v-model="nav" app>
+    <!--ITEMS DE NAVEGACION -->
+
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>Empresa</v-list-item-title>
+        <v-list-item-subtitle>Proinsalud</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider></v-divider>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "NavDrawer",
+  data: () => ({
+    nav: false,
+  }),
+};
+</script>
+
+<style scoped></style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <ToolbarNav />
+    <NavDrawer />
   </div>
 </template>
 
 <script lang="ts">
-import ToolbarNav from "../components/dashboard/ToolbarNav.vue";
+import ToolbarNav from "@/components/dashboard/ToolbarNav.vue";
+import NavDrawer from "@/components/dashboard/NavDrawer.vue";
 export default {
   name: "Dashboard",
   components: {
     ToolbarNav,
+    NavDrawer,
   },
 };
 </script>
