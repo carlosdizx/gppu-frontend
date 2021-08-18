b4f91f16d1e4c9dd0fab3fd6426d8259efac6708
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-17 19:19:28 -0500

    Solucionado inconveniente de comunicacion entre componentes 'hermanos'

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <ToolbarNav :nav="navigation" @changeDrawer="changeStateBar" />
-    <NavDrawer :isActive="navigation" @changeDrawer="itemSelector" />
+    <NavDrawer
+      :isActive="navigation"
+      @changeDrawer="itemSelector"
+      ref="NavDrawer"
+    />
     <h1 v-if="item === 1">componente Empresas</h1>
     <h1 v-if="item === 2">componente Supervisores</h1>
     <h1 v-if="item === 3">componente Estudiantes</h1>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import ToolbarNav from "@/components/dashboard/ToolbarNav.vue";
 import NavDrawer from "@/components/dashboard/NavDrawer.vue";
 import Vue from "vue";
@@ -23,12 +27,13 @@ export default Vue.extend({
     navigation: false,
   }),
   methods: {
-    itemSelector(idItem: any) {
+    itemSelector(idItem) {
       this.item = idItem;
       this.navigation = false;
     },
     changeStateBar() {
       this.navigation = !this.navigation;
+      this.$refs.NavDrawer.changeState();
     },
   },
 });
