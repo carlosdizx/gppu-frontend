1c94a64e2e5a15b584a7fa39828a54db134521c6
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-18 23:51:40 -0500

    Creado e implementado un calendario generico que permite registrar un rango de fechas

--- src/components/cruds/FormatoAprobatorio.vue
+++ src/components/cruds/FormatoAprobatorio.vue
@@ -11,37 +11,32 @@
           <span class="text-h5">Empresa {{ datos.nit }}</span>
         </v-card-title>
         <v-card-text>
-          <v-container>
-            <v-form>
-              <v-alert class="text-center" dense dark color="secondary">
-                Datos de la empresa
-              </v-alert>
-              <v-text-field label="Nit" :value="datos.nit" disabled />
-              <v-text-field label="Nombre" :value="datos.nombre" disabled />
-              <v-text-field
-                label="Representante"
-                :value="datos.documento"
-                disabled
-              />
-              <v-text-field label="Celular" :value="datos.celular" disabled />
-              <v-text-field label="Correo" :value="datos.correo" disabled />
-              <v-text-field label="Pais" :value="datos.pais" disabled />
-              <v-text-field
-                label="Departamento"
-                :value="datos.departamento"
-                disabled
-              />
-              <v-text-field label="Ciudad" :value="datos.ciudad" disabled />
-              <v-text-field
-                label="Direccion"
-                :value="datos.direccion"
-                disabled
-              />
-              <v-alert class="text-center" dense dark color="secondary">
-                Periodo de valides del convenio
-              </v-alert>
-            </v-form>
-          </v-container>
+          <v-form>
+            <v-alert class="text-center" dense dark color="secondary">
+              Datos de la empresa
+            </v-alert>
+            <v-text-field label="Nit" :value="datos.nit" disabled />
+            <v-text-field label="Nombre" :value="datos.nombre" disabled />
+            <v-text-field
+              label="Representante"
+              :value="datos.documento"
+              disabled
+            />
+            <v-text-field label="Celular" :value="datos.celular" disabled />
+            <v-text-field label="Correo" :value="datos.correo" disabled />
+            <v-text-field label="Pais" :value="datos.pais" disabled />
+            <v-text-field
+              label="Departamento"
+              :value="datos.departamento"
+              disabled
+            />
+            <v-text-field label="Ciudad" :value="datos.ciudad" disabled />
+            <v-text-field label="Direccion" :value="datos.direccion" disabled />
+            <v-alert class="text-center" dense dark color="secondary">
+              Periodo de valides del convenio
+            </v-alert>
+            <CalendarioRango />
+          </v-form>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
@@ -55,8 +50,10 @@
 </template>
 
 <script>
+import CalendarioRango from "../general/CalendarioRango";
 export default {
   name: "FormatoAprobatorio",
+  components: { CalendarioRango },
   data: () => ({
     dialog: false,
   }),
