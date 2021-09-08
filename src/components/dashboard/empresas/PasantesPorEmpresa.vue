2b6864dc7bb772a9b0de94ca31a4e2afa28e8adf
carlosdizx <carlodiaz@umariana.edu.co>
2021-09-07 19:31:19 -0500

    Creada la tabla dentro de un dialogo dinamico para los pasantes de las empresas aprobadas

---
+++ src/components/dashboard/empresas/PasantesPorEmpresa.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="600">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn fab small color="info darken-3" v-bind="attrs" v-on="on">
+        <v-icon>mdi-account-supervisor-outline</v-icon>
+      </v-btn>
+    </template>
+    <v-data-table :headers="columnas" :items="pasantes" class="elevation-1">
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Estudiante practicantes</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+      </template>
+    </v-data-table>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "PasantesPorEmpresa",
+  data: () => ({
+    columnas: [
+      { text: "Documento", value: "documento" },
+      { text: "Nombres", value: "nombres" },
+      { text: "Apellidos", value: "apellidos" },
+      { text: "Telefono", value: "telefono", sortable: false },
+      { text: "Correo", value: "correo", sortable: false },
+      { text: "Mas detalles", value: "detalle" },
+    ],
+  }),
+  props: {
+    pasantes: Array
+  }
+};
+</script>
+
+<style scoped></style>
