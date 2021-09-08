68ed637306142541bbbec2c212c0b1eb4a7ff432
carlosdizx <carlodiaz@umariana.edu.co>
2021-09-08 10:33:20 -0500

    Creado componente que permitira renovar el convenio de una empresa aprobada

---
+++ src/components/dashboard/empresas/DocumentoRenovacionConvenio.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="600">
+    <v-btn color="red darken-4" dark @click="dialog = !dialog">
+      <v-icon>mdi-close</v-icon>
+    </v-btn>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn fab small color="info darken-3" v-bind="attrs" v-on="on">
+        <v-icon>mdi-handshake-outline</v-icon>
+      </v-btn>
+    </template>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "DocumentoRenovacionConvenio",
+  data: () => ({
+    dialog: false,
+  }),
+};
+</script>
+
+<style scoped></style>
