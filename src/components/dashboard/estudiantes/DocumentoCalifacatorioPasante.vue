d898ba219f05dcd2a12088409d87a11b50baf69e
carlosdizx <carlodiaz@umariana.edu.co>
2021-09-14 23:30:52 -0500

    Se carga la empresa del estudiante que realiza la practica,esto para cada pasante

--- src/components/dashboard/estudiantes/DocumentoCalifacatorioPasante.vue
+++ src/components/dashboard/estudiantes/DocumentoCalifacatorioPasante.vue
@@ -11,6 +11,13 @@
     <v-card>
       <v-card-text>
         <v-form>
+          <v-alert dense color="blue-grey" dark>Datos estudiante</v-alert>
+          <v-text-field label="Nombres" disabled v-model="datos.nombres" />
+          <v-text-field label="Apellidos" disabled v-model="datos.apellidos" />
+          <v-text-field label="Doumento" disabled v-model="datos.documento" />
+          <v-alert dense color="blue-grey" dark>Datos empresa</v-alert>
+          <v-text-field label="Nombre" disabled v-model="empresa.nombre" />
+          <v-text-field label="Nombre" disabled v-model="empresa.nit" />
           <v-select
             label="Seleccione el calificativo"
             :items="opciones"
@@ -30,15 +37,32 @@
 </template>
 
 <script>
+import { LISTAR_EMPRESAS_APROBADAS } from "../../../services/recursos/empresaRS";
+
 export default {
   name: "DocumentoCalifacatorioPasante",
   data: () => ({
     dialog: false,
     opciones: ["Aprobado", "Reprobado", "Cancelado"],
     opcion: null,
+    empresa: {},
   }),
   props: {
     datos: Object,
+  },
+  async mounted() {
+    const resultado = await LISTAR_EMPRESAS_APROBADAS();
+    const empresas = Object.values(resultado.data);
+    for (const empresa of empresas) {
+      const pasantes = empresa.pasantes;
+      if (pasantes) {
+        for (const pasante of pasantes) {
+          if (this.datos.documento === pasante.documento) {
+            this.empresa = empresa;
+          }
+        }
+      }
+    }
   },
 };
 </script>
