b86ecafd85ba4db466d49bfd446fb7dcb60d0704
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-09-17 18:15:09 -0500

    Full correcciones para la ortografía

--- src/components/dashboard/egresados/ListadoEgresados.vue
+++ src/components/dashboard/egresados/ListadoEgresados.vue
@@ -37,7 +37,7 @@ export default {
       { text: "Apellidos", value: "apellidos" },
       { text: "Celular", value: "celular", sortable: false },
       { text: "Correo", value: "correo", sortable: false },
-      { text: "Mas detalles", value: "detalle" },
+      { text: "Más detalles", value: "detalle" },
     ],
     filas: [],
   }),
