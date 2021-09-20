788aff625a3c2d836780eaa1e7829177c0360232
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-09-19 19:14:22 -0500

    Creacion de la cabecera para el about para almacenar links e informacion relevante. ademas se agregaron unos videos de prueba dentro de la vista

---
+++ src/components/about/Cabecera.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-toolbar color="cyan darken-4" dark prominent>
+      <v-toolbar-title>Información GPPU</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <router-link to="/" v-slot="{ navigate }" custom>
+        <v-btn icon @click="navigate">
+          <v-icon>mdi-login</v-icon>
+        </v-btn>
+      </router-link>
+
+      <router-link to="/registro" v-slot="{ navigate }" custom>
+        <v-btn icon @click="navigate">
+          <v-icon>mdi-account-plus</v-icon>
+        </v-btn>
+      </router-link>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cabecera",
+};
+</script>
+
+<style scoped></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,23 +1,32 @@
 <template>
-  <v-container>
-    <v-card>
-      <v-card-title>Mas informacion</v-card-title>
-      <v-card-subtitle>Mas informacion</v-card-subtitle>
-      <v-card-text>
-        <h1>info 1</h1>
-        <h2>info 2</h2>
-        <h3>info 3</h3>
-        <h4>info 4</h4>
-        <h5>info 5</h5>
-        <h6>info 6</h6>
-      </v-card-text>
-    </v-card>
-  </v-container>
+  <div>
+    <Cabecera />
+    <v-container>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/hq2oqbslNus"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      />
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/_U3sLiAP9Fo"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      />
+    </v-container>
+  </div>
 </template>
 
 <script>
+import Cabecera from "../components/about/Cabecera";
 export default {
   name: "About",
+  components: { Cabecera },
 };
 </script>
 
