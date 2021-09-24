76f48325b3ef8bfc618cd2a13bcf20b3a8c34f1a
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-09-23 23:21:55 -0500

    Se quitaron algunos datos de ejemplo para tener un entorno ya de despligue final, despues se quitaran todos los campos

--- src/components/forms/FormExpressEmpresa.vue
+++ src/components/forms/FormExpressEmpresa.vue
@@ -163,10 +163,10 @@ export default {
   },
   data: () => ({
     dialog: false,
-    nit: "87656565-ak",
-    nombre: "Arenilla Gamer",
-    telefono: 313221632,
-    correo: "carlodiaz@umariana.edu.co",
+    nit: "",
+    nombre: "",
+    telefono: null,
+    correo: "",
     checkbox: false,
   }),
   methods: {
@@ -185,6 +185,7 @@ export default {
               "por llamada o correo respecto al proceso de convenio empresa universidad.",
             "success"
           );
+          this.dialog = !this.dialog;
           //await router.push("/about");
         }
       });
