83459974180d46e3e7e3eb9dea0c4d0bebd7cf5d
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-10-03 14:00:20 -0500

    Creado un cuerpo donde puede ir info de la pagina del about

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,31 +2,17 @@
   <div>
     <Cabecera />
     <v-container>
-      <iframe
-        width="100%"
-        height="300"
-        src="https://www.youtube.com/embed/hq2oqbslNus"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      />
-      <iframe
-        width="100%"
-        height="300"
-        src="https://www.youtube.com/embed/_U3sLiAP9Fo"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      />
+      <Cuerpo />
     </v-container>
   </div>
 </template>
 
 <script>
 import Cabecera from "../components/about/Cabecera";
+import Cuerpo from "../components/about/Cuerpo";
 export default {
   name: "About",
-  components: { Cabecera },
+  components: { Cabecera, Cuerpo },
 };
 </script>
 
