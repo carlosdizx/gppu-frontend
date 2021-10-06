b0ddf37571c91a7b556aaad9c15e2651a50d74f5
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-10-06 18:59:33 -0500

    Finalizando desacople, ademas se agrego un nuevo campo del fomulario con las habilidades que puede elegir para el desempeño de su practica profesional

--- src/views/Usuario.vue
+++ src/views/Usuario.vue
@@ -1,26 +1,16 @@
 <template>
   <div>
-    <v-card color="cyan darken-4">
-      <v-card-text>
-        <router-link to="dashboard" v-slot="{ navigate }" custom>
-          <v-btn icon @click="navigate" dark>
-            <v-icon>mdi-arrow-left</v-icon>
-          </v-btn>
-        </router-link>
-        <v-alert dense dark color="cyan darken-4">
-          Información del usuario
-        </v-alert>
-      </v-card-text>
-    </v-card>
+    <ToolbarUsuario />
     <FormUsuario />
   </div>
 </template>
 
 <script>
 import FormUsuario from "../components/usuario/FormUsuario";
+import ToolbarUsuario from "../components/usuario/ToolbarUsuario";
 export default {
   name: "Usuario",
-  components: { FormUsuario },
+  components: { FormUsuario, ToolbarUsuario },
 };
 </script>
 
