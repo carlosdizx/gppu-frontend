bf436ed85690e06f84cea2c0e8dc31a3a8236618
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-10-06 18:52:33 -0500

    Desacoplando de la vista de usuario a componentes

--- src/views/Usuario.vue
+++ src/views/Usuario.vue
@@ -12,80 +12,15 @@
         </v-alert>
       </v-card-text>
     </v-card>
-    <v-container>
-      <v-card-text>
-        <v-card>
-          <v-card-title>{{ rol }}</v-card-title>
-          <v-card-text>
-            <v-form autocomplete="off">
-              <v-alert dense dark>{{ programa }}</v-alert>
-              <v-text-field label="Nombres" v-model="nombres" />
-              <v-text-field label="Apellidos" v-model="apellidos" />
-              <v-text-field label="Documento" v-model="documento" />
-              <v-text-field type="number" label="Celular" v-model="celular" />
-              <v-text-field label="Dirección" v-model="direccion" />
-              <v-btn @click="registrar" color="success" block>
-                Registrar datos
-              </v-btn>
-            </v-form>
-          </v-card-text>
-        </v-card>
-      </v-card-text>
-    </v-container>
+    <FormUsuario />
   </div>
 </template>
 
 <script>
-import {
-  OBTENER_DATOS_USUARIO,
-  RISGISTAR_DATOS_USUARIO,
-} from "../services/auth";
-import Swal from "sweetalert2";
-
+import FormUsuario from "../components/usuario/FormUsuario";
 export default {
   name: "Usuario",
-  data: () => ({
-    nombres: "",
-    apellidos: "",
-    documento: "",
-    celular: "",
-    direccion: "",
-    programa: "",
-    rol: "",
-  }),
-  methods: {
-    async registrar() {
-      const usuario = {
-        nombres: this.nombres,
-        apellidos: this.apellidos,
-        documento: this.documento,
-        celular: this.celular,
-        direccion: this.direccion,
-      };
-      await RISGISTAR_DATOS_USUARIO(usuario).then((result) => {
-        if (result.status === 200) {
-          Swal.fire(
-            "Datos actualizados",
-            "Los datos de Coordinador@ de prácticas han sido actualizados",
-            "success"
-          );
-        }
-      });
-    },
-  },
-  async mounted() {
-    await OBTENER_DATOS_USUARIO().then((result) => {
-      if (result.data) {
-        this.nombres = result.data.nombres;
-        this.apellidos = result.data.apellidos;
-        this.documento = result.data.documento;
-        this.celular = result.data.celular;
-        this.direccion = result.data.direccion;
-        this.programa = result.data.programa;
-        this.rol = result.data.rol === 1 ? "Coordinador@ de prácticas" : "";
-      }
-    });
-  },
+  components: { FormUsuario },
 };
 </script>
 
