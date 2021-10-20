53df4bd53e8251d7315c129e57a3ece2c1fb70d4
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-10-20 09:50:34 -0500

    Se agrego texto informativo de la Universidad en el about

--- src/components/about/Footer.vue
+++ src/components/about/Footer.vue
@@ -1,44 +1,38 @@
 <template>
-  <v-footer dark padless>
-    <v-card flat tile class="cyan darken-4 lighten-1 white--text text-center">
-      <v-card-text>
-        <v-btn
-          v-for="item in items"
-          :key="item.url"
-          :href="item.url"
-          icon
-          role="link"
-          target="_blank"
-        >
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-btn>
-      </v-card-text>
+  <v-card
+    flat
+    dark
+    tile
+    class="cyan darken-4 lighten-1 white--text text-center"
+  >
+    <v-card-text>
+      <v-btn
+        v-for="item in items"
+        :key="item.url"
+        :href="item.url"
+        icon
+        role="link"
+        target="_blank"
+      >
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+    </v-card-text>
 
-      <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
-        Mauris cursus commodo interdum. Praesent ut risus eget metus luctus
-        accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a
-        sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula
-        lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus
-        iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor
-        vel ut orci. Orci varius natoque penatibus et magnis dis parturient
-        montes, nascetur ridiculus mus.
-      </v-card-text>
+    <v-card-text class="white--text pt-0">
+      La Universidad Mariana o Unimar es una universidad privada católica
+      colombiana ubicada en San Juan de Pasto. Cuenta con cinco facultades que
+      ofrecen un total de 20 programas de pregrado.
+    </v-card-text>
 
-      <v-divider></v-divider>
+    <v-divider></v-divider>
 
-      <v-card-text class="white--text">
-        © 2021 - 2026 Todos los derechos reservados. —
-        <v-btn
-          href="https://www.umariana.edu.co/index.html"
-          target="_blank"
-          text
-        >
-          <strong>Universidad Mariana</strong>
-        </v-btn>
-      </v-card-text>
-    </v-card>
-  </v-footer>
+    <v-card-text class="white--text">
+      © 2021 - 2026 Todos los derechos reservados. —
+      <v-btn href="https://www.umariana.edu.co/index.html" target="_blank" text>
+        <strong>Universidad Mariana</strong>
+      </v-btn>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
