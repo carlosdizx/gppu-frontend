3c08b006e28a4edb48166c1089f52a6410176be6
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-10-20 10:14:36 -0500

    Creado un listado de las empresas que ya forman un convenio con la empresa

---
+++ src/components/about/EmpresasConConvenio.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container>
+    <v-data-table :headers="columnas" :items="filas" class="elevation-1">
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>
+            Empresas que ya formalizaron uno o más convenio
+          </v-toolbar-title>
+        </v-toolbar>
+      </template>
+    </v-data-table>
+  </v-container>
+</template>
+
+<script>
+import { LISTAR_EMPRESAS_APROBADAS } from "../../services/recursos/empresaRS";
+export default {
+  name: "EmpresasConConvenio",
+  data: () => ({
+    columnas: [
+      { text: "Nit", value: "nit" },
+      { text: "Nombre", value: "nombre" },
+      { text: "País", value: "pais" },
+      { text: "Departamento", value: "departamento" },
+      { text: "Ciudad", value: "ciudad" },
+    ],
+    filas: [],
+  }),
+  methods: {
+    async cargarEmpresas() {
+      try {
+        await LISTAR_EMPRESAS_APROBADAS("lxWMzUUF0vZosLQtHyHvXNmztV52").then(
+          async (resultado) => {
+            if (resultado.data) {
+              this.filas = await Object.values(resultado.data);
+            }
+          }
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  mounted() {
+    this.cargarEmpresas();
+  },
+};
+</script>
+
+<style scoped></style>
