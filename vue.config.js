913e4211e9cec4ef9e035570afa3feb16319d4ce
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-10 12:25:17 -0500

    probando vergas

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/" + process.env.CI_PROJECT_NAME + "/"
+      : "/",
 };
