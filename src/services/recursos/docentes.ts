844e507e2b3a3a361deb2a1a1a5d0815701a2425
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-12 15:42:23 -0500

    Agregado los metodos de registro y comprobacion de docentes en el servcice

---
+++ src/services/recursos/docentes.ts
@@ -0,0 +1,31 @@
+import { INSTACIA } from "@/services/axios";
+import { STORAGE } from "@/main";
+
+export const REGISTRO_DOCENTE_PENDIENTE = async (
+  programa: any,
+  docente: any
+) =>
+  await INSTACIA.put(
+    `usuarios/${programa}/docentes/${docente.documento}.json`,
+    JSON.stringify(docente),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+export const ESTUDIANTE_YA_REGISTRADO = async (
+  programa: any,
+  documento: any
+) => {
+  const resultado = await INSTACIA.get(
+    `usuarios/${programa}/docentes/${documento}.json`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return resultado.data;
+};
