783af8cb690e12f9dc2b3bf16e1ace2cca3dad4a
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-18 10:11:28 -0500

    Agredao color en el despligue de la informacion

--- src/components/usuario/ToolbarUsuario.vue
+++ src/components/usuario/ToolbarUsuario.vue
@@ -1,20 +1,24 @@
 <template>
-  <v-card color="cyan darken-4">
+  <v-card :color="color">
     <v-card-text>
       <router-link to="dashboard" v-slot="{ navigate }" custom>
         <v-btn icon @click="navigate" dark>
           <v-icon>mdi-arrow-left</v-icon>
         </v-btn>
       </router-link>
-      <v-alert dense dark color="cyan darken-4">
-        Información del usuario
+      <v-alert dense dark :color="color">
+        <h1>Información del usuario</h1>
       </v-alert>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
   name: "ToolbarUsuario",
+  computed: {
+    ...mapState(["color"]),
+  },
 };
 </script>
